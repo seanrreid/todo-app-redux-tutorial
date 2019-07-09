1f594ea11557c2939337cf18b55fb9f73507815f
Sean R Reid <sean@torchcodelab.com>
2019-07-09 08:50:51 -0400

    Added ability to load Redux Dev Tools via the store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,10 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-export default createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+//export default createStore(rootReducer);
+export default store;
